d878c1d4d74d260788c1879d3061980a78a52eb7
UDAY-UGALE <udayugale68@gmail.com>
2026-06-25 23:03:50 +0530

    optimize the code

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,18 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          'react-vendor': ['react', 'react-dom'],
+          'motion': ['framer-motion'],
+          'gsap': ['gsap'],
+          'lenis': ['lenis'],
+        },
+      },
+    },
+  },
 })
